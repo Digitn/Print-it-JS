3d211693555c567aa28acdd7b93d5065eeec6357
abtn <digitalexb2tn@gmail.com>
2023-07-19 23:03:05 +0200

    Étape 2 : Ajout des écouteurs d'événements
    Ajout des Event Listeners sur chacune des flèches du slider avec différentiation d'événement pour chaque flèche et message console pour vérification

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,15 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+// Mise en place des écouteurs d'évènements lors du clic des boutons de flèches pour le changement d'image du slider
+const arrows = document.querySelectorAll('.arrow');
+arrows.forEach(function(arrow) {
+	arrow.addEventListener('click', function() {
+	if (arrow.classList.contains('arrow_left')) {
+		console.log('Le bouton "slide précédent" a été cliqué par l\'utilisateur'); // Message console pour afficher l'évènement
+	} else if (arrow.classList.contains('arrow_right')) {
+		console.log('Le bouton "slide suivant" a été cliqué par l\'utilisateur'); // Message console pour afficher l'évènement
+	}
+	});
+});
